f7d6f11aea3ce51b06edae879d7f96ddfb20a38b
Nguyên <macbook@MacBook-Air-cua-macbook.local>
2026-08-17 15:04:39 +0700

    feat: setup tokens

---
+++ src/app/onboard/page.tsx
@@ -0,0 +1,2 @@
+import Onboard from "@/components/components/Page/Onboard";
+export default Onboard;
---
+++ src/components/components/Page/Onboard.tsx
@@ -0,0 +1,10 @@
+function Onboard() {
+  return (
+    <main>
+      <h1>Delivo</h1>
+      <p>Order. Eat. Repeat</p>
+    </main>
+  );
+}
+
+export default Onboard;
