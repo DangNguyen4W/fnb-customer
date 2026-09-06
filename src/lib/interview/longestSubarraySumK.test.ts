5b36fd52df800af736a312c05914640d2c09439c
Nguyên <macbook@MacBook-Air-cua-macbook.local>
2026-09-06 12:11:36 +0700

    build: added commit messgae checking

---
+++ src/lib/interview/longestSubarraySumK.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { longestSubarraySumK } from "./longestSubarraySumK";
+
+describe("Test longestSubarraySumK", () => {
+  it.each([
+    [[1, -1, 5, -2, 3], 3, 4],
+    [[-2, -1, 2, 1], 1, 2],
+    [[1, 2, 3], 3, 2],
+    [[1, 2, 3], 100, 0],
+    [[], 0, 0],
+    [[0], 0, 1],
+    [[0, 0, 0], 0, 3],
+    [[1, -1, 1, -1, 1], 0, 4],
+    [[3], 3, 1],
+    [[-1, -1, -1, 1, 1], -1, 5],
+    [[1, 2, -2, 4, -4], 0, 41],
+  ])("Array: %j, k: %i -> output: %i", (array, k, output) => {
+    expect(longestSubarraySumK(array, k)).toBe(output);
+  });
+});
--- src/lib/interview/longestSubarraySumK.ts
+++ src/lib/interview/longestSubarraySumK.ts
@@ -1,5 +1,4 @@
-/* eslint-disable no-console */
-function longestSubarraySumK(nums: number[], target: number): number {
+export function longestSubarraySumK(nums: number[], target: number): number {
   const length = nums.length;
   for (let countFrom = 0; countFrom < length; countFrom++) {
     let sum = 0;
@@ -18,17 +17,3 @@ function longestSubarraySumK(nums: number[], target: number): number {
 
   return 0;
 }
-
-export const testLongestSubarraySumK = () => {
-  console.log(longestSubarraySumK([1, -1, 5, -2, 3], 3)); // 4
-  console.log(longestSubarraySumK([-2, -1, 2, 1], 1)); // 2
-  console.log(longestSubarraySumK([1, 2, 3], 3)); // 2
-  console.log(longestSubarraySumK([1, 2, 3], 100)); // 0
-  console.log(longestSubarraySumK([], 0)); // 0
-  console.log(longestSubarraySumK([0], 0)); // 1
-  console.log(longestSubarraySumK([0, 0, 0], 0)); // 3
-  console.log(longestSubarraySumK([1, -1, 1, -1, 1], 0)); // 4
-  console.log(longestSubarraySumK([3], 3)); // 1
-  console.log(longestSubarraySumK([-1, -1, -1, 1, 1], -1)); // 5
-  console.log(longestSubarraySumK([1, 2, -2, 4, -4], 0)); // 4
-};
